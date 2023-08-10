a25e773bd06640ee0c8e30eb4d606467f2b5f844
Douglas Soares <douglas.srs12@gmail.com>
2023-08-10 12:41:18 -0300

    Update Tools.php

--- src/Counties/M3530805/Tools.php
+++ src/Counties/M3530805/Tools.php
@@ -76,11 +76,8 @@ class Tools extends ToolsBase
             'xmlns:sis1'    => 'xmlns:sis1="">'
         ],
         2 => [
-            'xmlns:soap12' => "http://schemas.xmlsoap.org/soap/envelope/",
-            'xmlns:sis' => 'http://www.sistema.com.br/Sistema.Ws.Nfse',
-            'xmlns:nfse'    => "http://www.sistema.com.br/Nfse/arquivos/nfse_3.xsd",
-            'xmlns:xd' => 'http://www.w3.org/2000/09/xmldsig#',
-            'xmlns:sis1'    => 'xmlns:sis1="">'
+            'xmlns:soapenv' => "http://schemas.xmlsoap.org/soap/envelope/",
+            'xmlns:nfse'    => "https://testemogimirimabrasf.meumunicipio.online/ws/nfse"
         ]        
     ];
 }
