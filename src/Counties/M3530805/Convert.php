2243cd97260c86d54963d6a3cf29089b4f217b76
Douglas Soares <douglas.srs12@gmail.com>
2023-08-10 09:03:00 -0300

    Teste

--- src/Counties/M3530805/Convert.php
+++ src/Counties/M3530805/Convert.php
@@ -5,20 +5,20 @@ namespace NFePHP\NFSe\Counties\M3530805;
 /**
  * Classe a construção do xml da NFSe
  * para a Cidade de Mogi Mirim - SP
- * conforme o modelo SIGISS
+ * conforme o modelo ABRASF
  *
  * @category  NFePHP
  * @package   NFePHP\NFSe\Counties\M3530805\Convert
- * @copyright NFePHP Copyright (c) 2023
+ * @copyright NFePHP Copyright (c) 2016
  * @license   http://www.gnu.org/licenses/lgpl.txt LGPLv3+
  * @license   https://opensource.org/licenses/MIT MIT
  * @license   http://www.gnu.org/licenses/gpl.txt GPLv3+
- * @author    Matheus Felipe Marinho matheus_fellip@hotmail.com
+ * @author    Lucas B. Simões <lucas_development at outlook dot com>
  * @link      http://github.com/nfephp-org/sped-nfse for the canonical source repository
  */
 
-use NFePHP\NFSe\Models\SIGISS\Convert as ConvertSIGISS;
+use NFePHP\NFSe\Models\Abrasf\Convert as ConvertAbrasf;
 
-class Convert extends ConvertSIGISS
+class Convert extends ConvertAbrasf
 {
 }
--- src/Counties/M3530805/Tools.php
+++ src/Counties/M3530805/Tools.php
@@ -5,22 +5,22 @@ namespace NFePHP\NFSe\Counties\M3530805;
 /**
  * Classe para a comunicação com os webservices da
  * Cidade de Mogi Mirim SP
- * conforme o modelo SIGISS
+ * conforme o modelo ABRASF
  *
  * @category  NFePHP
  * @package   NFePHP\NFSe\Counties\M3530805\Tools
- * @copyright NFePHP Copyright (c) 2023
+ * @copyright NFePHP Copyright (c) 2016
  * @license   http://www.gnu.org/licenses/lgpl.txt LGPLv3+
  * @license   https://opensource.org/licenses/MIT MIT
  * @license   http://www.gnu.org/licenses/gpl.txt GPLv3+
- * @author    Matheus Felipe Marinho <matheus@gmail.com>
+ * @author    Lucas B. Simões <lucas_development at outlook dot com>
  * @link      http://github.com/nfephp-org/sped-nfse for the canonical source repository
  */
 
-use NFePHP\NFSe\Models\SIGISS\SoapCurl;
-use NFePHP\NFSe\Models\SIGISS\Tools as ToolsSIGISS;
+use NFePHP\NFSe\Models\Abrasf\Factories;
+use NFePHP\NFSe\Models\Abrasf\Tools as ToolsAbrasf;
 
-class Tools extends ToolsSIGISS
+class Tools extends ToolsAbrasf
 {
     /**
      * Webservices URL
@@ -34,7 +34,7 @@ class Tools extends ToolsSIGISS
      * County Namespace
      * @var string
      */
-    protected $xmlns = '';
+    protected $xmlns = 'http://www.e-governeapps2.com.br';
     /**
      * Soap Version
      * @var int
@@ -44,7 +44,7 @@ class Tools extends ToolsSIGISS
      * SIAFI County Cod
      * @var int
      */
-    protected $codcidade = 7667;
+    protected $codcidade = 6717;
     /**
      * Indicates when use CDATA string on message
      * @var boolean
@@ -59,48 +59,51 @@ class Tools extends ToolsSIGISS
      * Version of schemas
      * @var int
      */
-    protected $versao = 103;
+    protected $versao = 300;
 
     /**
      * Namespaces for soap envelope
      * @var array
      */
-    protected $namespaces = [
-        'xmlns:SOAP-ENV' => "http://schemas.xmlsoap.org/soap/envelope/",
-        'xmlns:xsd' => "http://www.w3.org/2001/XMLSchema",
-        'xmlns:xsi' => "http://www.w3.org/2001/XMLSchemainstance", 
-        'xmlns:SOAP-ENC' => 'http://schemas.xmlsoap.org/soap/encoding/', 
-        'xmlns:tns' => 'https://mogimirim.meumunicipio.online/abrasf/ws?wsdl'
-    ];
+    protected $namespaces = ['xmlns:soapenv' => "http://schemas.xmlsoap.org/soap/envelope/", 'xmlns:nfe' => "http://nfe.mogimirim.sp.gov.br"];
 
     protected $params = [];
 
     /**
      * @param $lote
-     * @param $rps
+     * @param $rpss
      * @return string
      */
-    public function gerarNota($rps)
+    public function recepcionarLoteRps($lote, $rpss)
     {
-        $class = "NFePHP\\NFSe\\Counties\\M4113700\\v{$this->versao}\\GerarNota";
+        $class = "NFePHP\\NFSe\\Counties\\M3530805\\v{$this->versao}\\RecepcionarLoteRps";
         $fact = new $class($this->certificate);
 
-        return $this->gerarNotaCommon($fact, $rps);
+        return $this->recepcionarLoteRpsCommon($fact, $lote, $rpss);
     }
 
     /**
-     * @param $fact
+     * @param Factories\RecepcionarLoteRps $fact
      * @param $lote
-     * @param $rps
+     * @param $rpss
      * @param string $url
      * @return string
      */
-    protected function gerarNotaCommon($fact, $rps, $url = '')
+    protected function recepcionarLoteRpsCommon(Factories\RecepcionarLoteRps $fact, $lote, $rpss, $url = '')
     {
-        $this->method = 'GerarNota';
+        $this->method = 'RecepcionarLoteRpsV3';
+        $fact->setXmlns($this->xmlns);
+        $fact->setSchemeFolder($this->schemeFolder);
+        $fact->setCodMun($this->config->cmun);
+        $fact->setSignAlgorithm($this->algorithm);
+        $fact->setTimezone($this->timezone);
         $message = $fact->render(
             $this->versao,
-            $rps
+            $this->remetenteTipoDoc,
+            $this->remetenteCNPJCPF,
+            $this->remetenteIM,
+            $lote,
+            $rpss
         );
 
         // @header ("Content-Disposition: attachment; filename=\"NFSe_Lote.xml\"" );
@@ -110,80 +113,110 @@ class Tools extends ToolsSIGISS
     }
 
     /**
-     * Consulta pelo Numero do RPS
-     * @param string $rps
+     * Consulta Lote
+     * @param string $protocolo
      * @return string
      */
-    public function consultarRps($rps)
+    public function consultarLoteRps($protocolo)
     {
-        $class = "NFePHP\\NFSe\\Counties\\M4113700\\v{$this->versao}\\ConsultarRps";
+        $class = "NFePHP\\NFSe\\Counties\\M3530805\\v{$this->versao}\\ConsultarLoteRps";
         $fact = new $class($this->certificate);
-        return $this->consultarRpsCommon($fact, $rps);
+        return $this->consultarLoteRpsCommon($fact, $protocolo);
     }
 
     /**
      * @param $fact
-     * @param $rps
+     * @param $protocolo
      * @param string $url
      * @return string
      */
-    protected function consultarRpsCommon($fact, $rps, $url = '')
+    protected function consultarLoteRpsCommon($fact, $protocolo, $url = '')
     {
-        $this->method = 'ConsultarRpsServicoPrestado';
-        $message = $fact->render($this->versao, $rps);
+        $this->method = 'ConsultarLoteRpsV3';
+        $message = $fact->render($this->versao,$this->remetenteCNPJCPF, $this->remetenteIM, $protocolo);
         return $this->sendRequest($url, $message);
     }
 
     /**
      * Consulta Lote
-     * @param string $rps
+     * @param string $protocolo
      * @return string
      */
-    public function consultarNfse($rps)
+    public function consultarSituacaoLoteRps($protocolo)
     {
-        $class = "NFePHP\\NFSe\\Counties\\M4113700\\v{$this->versao}\\ConsultarNfse";
+        $class = "NFePHP\\NFSe\\Counties\\M3530805\\v{$this->versao}\ConsultarSituacaoLoteRps";
         $fact = new $class($this->certificate);
-        return $this->consultarNfseCommon($fact, $rps);
+        return $this->consultarSituacaoLoteRpsCommon($fact, $protocolo);
     }
 
     /**
      * @param $fact
-     * @param string $rps
+     * @param $protocolo
      * @param string $url
      * @return string
      */
-    protected function consultarNfseCommon($fact, $rps, $url = '')
+    protected function consultarSituacaoLoteRpsCommon($fact, $protocolo, $url = '')
     {
-        $this->method = 'ConsultarNfseServicoPrestado';
-        $message = $fact->render($this->versao, $rps);
+        $this->method = 'ConsultarSituacaoLoteRpsV3';
+        $message = $fact->render($this->versao, $this->remetenteCNPJCPF, $this->remetenteIM, $protocolo);
         return $this->sendRequest($url, $message);
     }
 
-    
     /**
-     * Consulta Cadastro
-     * @param string $rps
-     * @param string $cpfCnpjContribuinte
+     * Consulta Lote
+     * @param string $numeroNfse
+     * @param string $dataInicial
+     * @param string $dataFinal
      * @return string
      */
-    public function ConsultarCadastro($rps, $cpfCnpjContribuinte)
+    public function consultarNfse($numeroNfse,$dataInicial,$dataFinal)
     {
-        $class = "NFePHP\\NFSe\\Counties\\M4113700\\v{$this->versao}\\ConsultarCadastro";
+        $class = "NFePHP\\NFSe\\Counties\\M3530805\\v{$this->versao}\\ConsultarNfse";
         $fact = new $class($this->certificate);
-        return $this->ConsultarCadastroCommon($fact, $rps, $cpfCnpjContribuinte);
+        return $this->consultarNfseCommon($fact, $numeroNfse,$dataInicial,$dataFinal);
     }
 
     /**
      * @param $fact
-     * @param string $rps
-     * @param string $cpfCnpjContribuinte
+     * @param string $numeroNfse
+     * @param string $dataInicial
+     * @param string $dataFinal
      * @param string $url
      * @return string
      */
-    protected function ConsultarCadastroCommon($fact, $rps, $cpfCnpjContribuinte, $url = '')
+    protected function consultarNfseCommon($fact, $numeroNfse,$dataInicial,$dataFinal, $url = '')
     {
-        $this->method = 'ConsultarCadastroContribuinte';
-        $message = $fact->render($this->versao, $rps, $cpfCnpjContribuinte);
+        $this->method = 'ConsultarNfseV3';
+        $message = $fact->render($this->versao, $this->remetenteCNPJCPF, $this->remetenteIM, $numeroNfse,$dataInicial,$dataFinal);
+        return $this->sendRequest($url, $message);
+    }
+
+
+    /**
+     * Consulta Lote
+     * @param string $numero
+     * @param string $serie
+     * @param string $tipo
+     * @return string
+     */
+    public function consultarNfsePorRpsV3($numero, $serie,$tipo)
+    {
+        $class = "NFePHP\\NFSe\\Counties\\M3530805\\v{$this->versao}\\ConsultarNfsePorRps";
+        $fact = new $class($this->certificate);
+        return $this->consultarNfsePorRpsCommon($fact, $numero, $serie,$tipo);
+    }
+
+    /**
+     * @param $fact
+     * @param $protocolo
+     * @param string $url
+     * @return string
+     */
+    protected function consultarNfsePorRpsCommon($fact, $numero, $serie,$tipo, $url = '')
+    {
+        $this->method = 'ConsultarNfsePorRpsV3';
+        $fact->setXmlns($this->xmlns);
+        $message = $fact->render($this->versao, $this->remetenteCNPJCPF, $this->remetenteIM, $numero, $serie,$tipo);
         return $this->sendRequest($url, $message);
     }
 
@@ -202,7 +235,7 @@ class Tools extends ToolsSIGISS
         }
 
         if (!is_object($this->soap)) {
-            $this->soap = new SoapCurl($this->certificate);
+            $this->soap = new \NFePHP\NFSe\Counties\M3530805\SoapCurl($this->certificate);
         }
 
         //formata o xml da mensagem para o padão esperado pelo webservice
@@ -215,21 +248,24 @@ class Tools extends ToolsSIGISS
 
         //O atributo xmlns precisa ser removido da tag <EnviarLoteRpsEnvio> pois
         //o web service de Itabira não o reconhece
-        $messageText = str_replace('xmlns="http://www.SIGISS.org.br/nfse.xsd"', '', $message);
+        $messageText = str_replace('xmlns="http://www.abrasf.org.br/nfse.xsd"', '', $message);
 
         if ($this->withcdata) {
             $messageText = $this->stringTransform($message);
         }
 
-        $request = '<tns:' . $this->method . ' xmlns:tns="https://mogimirim.meumunicipio.online/abrasf/ws?wsdl">' . trim($messageText) . '</tns:' . $this->method . '>';
+        $request = '<nfe:' . $this->method . '>
+                         <arg0><![CDATA[<ns2:cabecalho versao="3" xmlns:ns2="http://nfe.sjp.pr.gov.br/cabecalho_v03.xsd"><versaoDados>3</versaoDados></ns2:cabecalho>]]></arg0>
+             <arg1><![CDATA[' . $messageText . ']]></arg1>
+           </nfe:' . $this->method . '>';
 
         $this->params = array(
             "soapaction: '$url#" . $this->method . "'",
-            'Host: https://mogimirim.meumunicipio.online/abrasf/ws?wsdl',
+            'Host: nfe.sjp.pr.gov.br',
             'Content-Type: text/xml; charset=ISO-8859-1'
         );
 
-        $action = '';
+        $action = 'RecepcionarLoteRpsV3';
         //Realiza o request SOAP
         return $this->soap->send(
             $url,
@@ -244,17 +280,26 @@ class Tools extends ToolsSIGISS
 
     /**
      * Método que converto o objeto RPS em XML;
-     * @param \NFePHP\NFSe\Models\SIGISS\Rps $rps
+     * @param \NFePHP\NFSe\Models\Abrasf\Rps $rps
      * @return string Retorna o xml serializado
      */
     public function makeXml($rps)
     {
-        $class = "NFePHP\\NFSe\\Counties\\M4113700\\v{$this->versao}\\GerarNota";
+        $class = "NFePHP\\NFSe\\Counties\\M3530805\\v{$this->versao}\\RecepcionarLoteRps";
         $fact = new $class($this->certificate);
 
+        $fact->setXmlns($this->xmlns);
+        $fact->setSchemeFolder($this->schemeFolder);
+        $fact->setCodMun($this->config->cmun);
+        $fact->setSignAlgorithm($this->algorithm);
+        $fact->setTimezone($this->timezone);
         $message = $fact->render(
             $this->versao,
-            $rps
+            $this->remetenteTipoDoc,
+            $this->remetenteCNPJCPF,
+            $this->remetenteIM,
+            1,
+            [$rps]
         );
 
         $dom = new \DOMDocument('1.0', 'UTF-8');
@@ -266,7 +311,7 @@ class Tools extends ToolsSIGISS
 
         //O atributo xmlns precisa ser removido da tag <EnviarLoteRpsEnvio> pois
         //o web service de Itabira não o reconhece
-        $messageText = str_replace('xmlns="http://www.SIGISS.org.br/nfse.xsd"', '', $message);
+        $messageText = str_replace('<EnviarLoteRpsEnvio xmlns="http://nfe.sjp.pr.gov.br/servico_enviar_lote_rps_envio_v03.xsd">', '<EnviarLoteRpsEnvio>', $message);
 
         if ($this->withcdata) {
             $messageText = $this->stringTransform($message);
@@ -274,38 +319,41 @@ class Tools extends ToolsSIGISS
 
         return $messageText;
     }
-
-    public function cancelarNfse($rps, $codCancelamento, $email = '')
+    
+    public function cancelarNfse($nfseNumero)
     {
-        $class = "NFePHP\\NFSe\\Counties\\M4113700\\v{$this->versao}\\CancelarNota";
+        return '<?xml version="1.0" encoding="utf-8"?><ConsultarLoteRpsResponse xmlns="http://www.e-governeapps2.com.br/">
+                <ListaMensagemRetorno>
+                    <MensagemRetorno>
+                    <Codigo>E999</Codigo>
+                    <Mensagem>ESTA NOTA NAO PODERA SER CANCELADA CONFORME DECRETO No. 1.852, DE 28 DE AGOSTO 2014</Mensagem>
+                    <Correcao></Correcao>
+                    </MensagemRetorno>
+                </ListaMensagemRetorno>
+                </ConsultarLoteRpsResponse>';
+
+        /*$this->soapAction = $this->url[$this->config->tpAmb];
+
+        $class = "NFePHP\\NFSe\\Counties\\M3530805\\v{$this->versao}\\CancelarNfse";
         $fact = new $class($this->certificate);
 
-        return $this->cancelarNotaCommon($fact, $rps, $codCancelamento, $email);
-    }
-
-    /**
-     * @param $fact
-     * @param $rps
-     * @param $codCancelamento
-     * @param $email
-     * @param string $url
-     * @return string
-     */
-    protected function cancelarNotaCommon($fact, $rps, $codCancelamento, $email = '', $url = '')
-    {
-        $this->method = 'CancelarNota';
-        
+        $this->method = 'CancelarNfseV3';
+        $fact->xmlns = $this->xmlns;
+        $fact->schemeFolder = $this->schemeFolder;
+        $fact->codMun = $this->config->cmun;
+        $fact->algorithm = $this->algorithm;
+        //$fact->setTimezone($this->timezone);
         $message = $fact->render(
             $this->versao,
-            $rps, 
-            $codCancelamento,
-            $email
+            $this->remetenteTipoDoc,
+            $this->remetenteCNPJCPF,
+            $this->remetenteIM,
+            $nfseNumero
         );
-        
 
         // @header ("Content-Disposition: attachment; filename=\"NFSe_Lote.xml\"" );
         // echo $message;
         // exit;
-        return $this->sendRequest($url, $message);
+        return $this->sendRequest('', $message);*/
     }
 }
