64ca51a265446e6a6d72ddfc1a50d0d2dcb40dfd
Douglas Soares <douglas.srs12@gmail.com>
2023-08-10 12:31:34 -0300

    Update Tools.php

--- src/Models/Abrasf/Tools.php
+++ src/Models/Abrasf/Tools.php
@@ -183,8 +183,7 @@ class Tools extends ToolsBase
             default:
                 throw new \LogicException('Versão não suportada');
         }
-        //header("Content-type: text/xml");
-        die(print_r(htmlspecialchars($request, ENT_QUOTES), true));
+
         return $request;
     }
 
